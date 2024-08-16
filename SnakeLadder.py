8fa794cc6a927b2d161406af9479b1b382e63a20
poojangit <poojang147@gmail.com>
2024-08-16 14:35:01 +0530

    [POOJA] Added : Implemented 2md use case for generating a random dice value for the single user

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -1,7 +1,11 @@
-
+import random
 start_position = 0
 
+def roll_dice():
+    return random.randint(1,6)
 
 if __name__ == '__main__':
     print("Welcome to the Snake and ladder game")
+    dice_value = roll_dice()
+    print(f"The player rolled a {dice_value}")
     
