131872e0692e43d1c333ff60190e0e82edede2fa
poojangit <poojang147@gmail.com>
2024-08-16 15:16:49 +0530

    [POOJA] Added : Implemented 3rd usecase for checking snake or ladder and implemented its logic

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -1,11 +1,27 @@
 import random
 start_position = 0
+current_postion = start_position
 
 def roll_dice():
     return random.randint(1,6)
+
+def check_option():
+    return random.choice(["no play", "ladder", "snake"])
 
 if __name__ == '__main__':
     print("Welcome to the Snake and ladder game")
     dice_value = roll_dice()
     print(f"The player rolled a {dice_value}")
+    random_option = check_option()
+    print(f"The player encounterred {random_option}")
     
+    if random_option == "no play":
+        print(f"NO PLAY : The player will stay at the same position {current_postion} ")
+    elif random_option == "ladder":
+        current_postion += dice_value
+        print(f"LADDER : The player will move ahead of dice value current postion is {current_postion}")
+    elif random_option == "snake" :
+        current_postion -=dice_value
+        if current_postion < 0:
+            current_postion = 0
+        print(f"SNAKE: The player moves behind now current position is {current_postion}")
