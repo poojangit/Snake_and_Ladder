3469292dcbb9d40764e779446aa6879eebd8e571
poojangit <poojang147@gmail.com>
2024-08-16 15:33:28 +0530

    [POOJA] Added : Implemented 4th use case for having condition of winning postion 100

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -1,27 +1,48 @@
 import random
+
 start_position = 0
-current_postion = start_position
+end_position = 100
+
 
 def roll_dice():
-    return random.randint(1,6)
+    return random.randint(1, 6)
+
 
 def check_option():
     return random.choice(["no play", "ladder", "snake"])
 
-if __name__ == '__main__':
+
+def snake_ladder_game():
+    current_postion = start_position
     print("Welcome to the Snake and ladder game")
-    dice_value = roll_dice()
-    print(f"The player rolled a {dice_value}")
-    random_option = check_option()
-    print(f"The player encounterred {random_option}")
-    
-    if random_option == "no play":
-        print(f"NO PLAY : The player will stay at the same position {current_postion} ")
-    elif random_option == "ladder":
-        current_postion += dice_value
-        print(f"LADDER : The player will move ahead of dice value current postion is {current_postion}")
-    elif random_option == "snake" :
-        current_postion -=dice_value
-        if current_postion < 0:
-            current_postion = 0
-        print(f"SNAKE: The player moves behind now current position is {current_postion}")
+
+    while current_postion < end_position:
+        # input("Enter to roll the dice")
+        dice_value = roll_dice()
+        print(f"The player rolled a {dice_value}")
+
+        random_option = check_option()
+        print(f"The player encounterred {random_option}")
+
+        if random_option == "no play":
+            print(
+                f"NO PLAY : The player will stay at the same position {current_postion} "
+            )
+        elif random_option == "ladder":
+            current_postion += dice_value
+            print(
+                f"LADDER : The player will move ahead of dice value current postion is {current_postion}"
+            )
+        elif random_option == "snake":
+            current_postion -= dice_value
+            if current_postion < 0:
+                current_postion = 0
+            print(
+                f"SNAKE: The player moves behind now current position is {current_postion}"
+            )
+            
+        print(f"Current postion : {current_postion}")
+        print("---------------------------------------")
+
+if __name__ == "__main__":
+    snake_ladder_game()
